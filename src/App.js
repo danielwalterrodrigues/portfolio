35a2044e986ebd79c17559f756bce4a3137ab18d
danielwalterrodrigues <danielwalterrodrigues@gmail.com>
2022-10-02 05:21:37 -0300

    subindo os arquivos iniciais

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import logoGrande from './assets/logo_branco.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="laranja"></div>
+      <div className="cinza"></div>
+      <div className="branco">
+        <img src={logoGrande} alt=''></img>
+        Logo 
+      </div>
+      <div className="preto"></div>
+    </>
   );
 }
 
