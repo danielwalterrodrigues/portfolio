5e4bd9677e6c23f9eebb01703a8542a9f7f0f8c6
danielwalterrodrigues <danielwalterrodrigues@gmail.com>
2022-10-06 04:12:13 -0300

    motion framer changes

--- src/components/redessociais.js
+++ src/components/redessociais.js
@@ -1,22 +1,37 @@
 import '../App.css';
+import { motion } from "framer-motion";
 
+const telefone = "11536706";
 function redesSociais() {
 
     return (
         
         <>
         <div className='social_preto_separacao'>
-            <div className='social_preto_rodape'>E</div>
-            <div className='social_preto_rodape'>Q</div>
-            <div className='social_preto_rodape'>C</div>
-            <div className='social_preto_rodape'>e</div>
-            <div className='social_preto_rodape'>M</div>
-            <div className='social_preto_rodape'>k</div>
-            <div className='social_preto_rodape'>L</div>
+            <motion.div className='social_preto_rodape' initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.1, ease: [0, 0.71, 0.2, 1.01]}}>E</motion.div>
+            <motion.div className='social_preto_rodape' initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.2, ease: [0, 0.71, 0.2, 1.01]}}>Q</motion.div>
+            <motion.div className='social_preto_rodape' initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.3, ease: [0, 0.71, 0.2, 1.01]}}>C</motion.div>
+            <motion.div className='social_preto_rodape' initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.4, ease: [0, 0.71, 0.2, 1.01]}}>e</motion.div>
+            <motion.div className='social_preto_rodape' initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.5, ease: [0, 0.71, 0.2, 1.01]}}>M</motion.div>
+            <motion.div className='social_preto_rodape' initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.6, ease: [0, 0.71, 0.2, 1.01]}}>k</motion.div>
+            <motion.div className='social_preto_rodape' initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.7, ease: [0, 0.71, 0.2, 1.01]}}>L</motion.div>
         </div>
         <div className='social_preto_separacao'>
-            <div className='whatsapp'>+55 11 95367 - 0683</div>
-        </div>
+            <motion.div
+                className="whatsapp"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.8,
+                    delay: 0.5,
+                    ease: [0, 0.71, 0.2, 1.01]
+                }}
+                >
+                {"11 95367 - 0683"}
+                </motion.div>
+                    
+        </div>                
+        
         </>
     )
 }
