eaddc1e7af017b8efe47f3860303b33034d98602
danielwalterrodrigues <danielwalterrodrigues@gmail.com>
2022-12-09 23:57:46 -0300

    ajustes DEV

--- src/components/Dev/Dev.js
+++ src/components/Dev/Dev.js
@@ -9,13 +9,22 @@ import imagemteste from '../../assets/4real/4real3.png';
 import {db} from '../../firebase';
 import { useState, useEffect } from 'react';
 import { collection, getDocs } from "firebase/firestore"; 
+import Detalhe from './detalhe';
 
 const Dev = () => {
+    const [detalhe, AbreDetalhe] = useState(false);
     // esses states que vão receber os resultados do fecth
         
     const [todosDocs, setTodosDocs] = useState([]); // usa-se o [] pra array
     //const [umdoc, setUmdoc] = useState({}); // usae {} pq é um objeto
     
+    const funcDetalhe = event => {
+        AbreDetalhe(true);
+    }
+    const fechaDetalhe = event => {
+        AbreDetalhe(false);
+    }
+
     useEffect(() => {
 
         getDocs(collection(db, "projetos")).then((querysnapshot) => { // colecao projetos tá aqui e o then é um snapshot
@@ -33,7 +42,7 @@ const Dev = () => {
     return (
     <>
     <Titulo corcomplementar="#440062" Nome="DESENVOLVEDOR" />
-    <div className='loguinhos'><img src={devloguinhos} style={{marginLeft: 10, marginTop: 80,}}></img></div>
+    {/* <div className='loguinhos'><img src={devloguinhos} style={{marginLeft: 10, marginTop: 80,}}></img></div> */}
     <motion.div className="brancoGeral" id="flex"
             animate={{ x:'20px', opacity: 1}}
             transition={{ type: 'spring', restSpeed: 1.5 }}
@@ -41,10 +50,20 @@ const Dev = () => {
                 
 {todosDocs.map((doc)=>{
 return(
+    <>
+    {detalhe && 
+        <>
+            <Detalhe />
+            <button onClick={fechaDetalhe} className='detalheFechar'>
+                fechar
+            </button>
+        </>
+    }
+    
         <div className='card'>
         <div className='BoxProjeto' style={{order:(Math.floor(Math. random()*(1-170)+170))}}>
                 <div class="content" style={{height:((Math. random()*(37-5)+5)*10),}}>
-                    <div class="front" style={{backgroundImage: 'url('+doc.Imagens[2]+')', backgroundSize: '120%',}}>
+                    <div class="front" style={{backgroundImage: 'url('+doc.Imagens[0]+')', backgroundSize: 'cover',}}>
                         <div className='cardBranco'>
                         </div>
                         <div className='title'>{doc.Nome}</div>
@@ -57,12 +76,14 @@ return(
                             {doc.DescricaoCurta}
                         </p>
                         <div className='cardlogo'>
-                            <button className='Mais'>+</button>
+                            <button className='Mais' onClick={funcDetalhe}>+</button>
+                            
                         </div>
                     </div>
                 </div>
         </div>
         </div>
+    </>
 )}
 )}
 
---
+++ src/components/Dev/detalhe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './detalhe.css';
+
+const Detalhe = (props) => {
+
+    return (
+        <>
+            <div className='detalhe'>
+                Bom dia, amiguinhos eu estou aqui
+            </div>
+        </>
+    )
+}
+export default Detalhe
